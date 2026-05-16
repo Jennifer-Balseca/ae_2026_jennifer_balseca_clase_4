3a6cc61e75632f0db0f3e159746c4dc3e8a9ec29
Estudiante LTIC <Estudiante@ltic.local>
2026-05-16 10:43:01 -0500

    ae_2026_jennifer_balseca_clase_4_1 main.kt

--- main.kt
+++ main.kt
@@ -1,16 +1,10 @@
 fun main() {
-    val name: String = "Ana"
-    val email: String = "ana@gmail.com"
-    val number: Int = 67
-    //Ana -ana@gmail.com
+    //siempre debo tipar mis variables (o constantes)
+    val mensaje: String = "Hola"
+    var variableMutable: Int = 5
+    println(mensaje)
+    println(variableMutable)
     
-    //y evitar agregacion de string, ocupa mucha ram a la larga:
-    println(name + "-" + email)
-    //siempre usar string templates ${}:
-    println("$name - $email")
-    
-    //se pueden hacer cálculos steven con string pequeños
-	println("Te faltan ${100 - number} para llegar a 100")
-    
-    
+    variablemutable =10
+    println(variableMutable)
 }
