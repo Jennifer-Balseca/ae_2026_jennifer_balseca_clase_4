630d0ddf6a9fb07f7ef8c2e574d26ecb3d2048b1
Estudiante LTIC <Estudiante@ltic.local>
2026-05-16 10:58:00 -0500

    clase_4_4

--- main.kt
+++ main.kt
@@ -1,19 +1,27 @@
+
+  //hay muchos tipos de clases, vamos a usar mucho =data class : abstracción que nos sirve para representar múltiples valores a un solo objeto
+  //construcción de la clasefuncion por defecto que permite crear una nueva instancia de la clase
+
+data class Student(
+    val id: Long,
+    val name: String,
+    val email: String,
+    val grade: Int,
+    val isActive: Boolean
+)
+
 fun main() {
-    val grade: Int = 85
+    //creación de instancias de clases:
+    val jorge = Student(
+ 		id = 1,
+        name = "Jorge",
+        email = "jorge@puce.edu.ec",
+        grade = 8, 
+        isActive = true
+    ) //no importa el orden del contructor, lo importante es que esté todo lo que pide
     
-    if (grade > 70) {
-        println("Aprobado")
-    } else {
-        println("Reprobado")
-    }
-    
-    //se puede usar if como una expresión de variable
-   	//se puede reducir y mejorar
-    val results: String = if (grade > 70){
-        "Aprobado"
-    } else {
-        "Reprobado"
-    }
-		
-    println(results)
+    val ana = Student(1, "Ana", "ana@puce.edu.ec", 10, true) //si se usa una instancia simple si hay que respetar el orden del constructor
+    println(ana)
+    println(jorge)
+   
 }
